779c1ec4ecdf73832d8384ee886effa42e40cf69
Arun <arunsenapati407@gmail.com>
2026-08-10 16:49:10 +0000

    Check Number is Strong number or not

---
+++ C/Basics/StrongNumber.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+    int n, q, rem, fact=1, i, result=0;
+    printf("Please enter a number: ");
+    scanf("%d", &n);
+
+    q = n;
+    while( q != 0){
+        rem = q % 10;
+        for(int i = 1; i<=rem; i++){
+            fact = fact * i;
+        }
+        result += fact;
+        fact = 1;
+        q /= 10;
+    }
+
+    if(result == n){
+        printf("%d is a strong number\n", n);
+    }else{
+        printf("%d is not a strong number\n", n);
+    }
+    return 0;
+}

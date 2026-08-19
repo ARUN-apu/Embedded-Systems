3cdcfd03e1a8595d1136a0fe729fe2fbf2749932
Arun <arunsenapati407@gmail.com>
2026-08-19 10:17:45 +0000

    Reversing a series of numbers using pointers

---
+++ C/Pointers/Problem3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define N 5
+
+int main(){
+    int a[N], *p;
+    printf("Enter %d elements in the array: \n", N);
+    for(p = a; p <= a+N-1; p++){
+        scanf("%d", p);
+    }
+
+    printf("Elements in reverse order:\n");
+    for(p = a+N-1; p>= a; p--){
+        printf("%d ",*p);
+    }
+
+    printf("\n");
+
+    return 0;
+}

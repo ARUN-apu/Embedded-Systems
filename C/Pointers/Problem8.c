891419e438202d0b49bcf1f899e9aeede5348d1a
Arun <arunsenapati407@gmail.com>
2026-08-21 11:06:22 +0000

    Problem 8

---
+++ C/Pointers/Problem8.c
@@ -0,0 +1,25 @@
+// What should be the contents of the array b at the end of the program?
+
+#include <stdio.h>
+
+int main(){
+    int i,j;
+    char a[2][3] = {{'a','b','c'}, {'d','e','f'}};
+    char b[3][2];
+    char *p = *b;
+    for(i = 0; i < 2; i++){
+        for(j = 0; j < 3; j++){
+            *(p + 2*j + i) = a [i][j];
+        }
+    }
+
+     printf("The contents of array b are:\n");
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 2; j++){
+            printf("%c ", b[i][j]);
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}

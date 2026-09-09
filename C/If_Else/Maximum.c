2733a22056217e29adc79bf42fa2e22449bdb600
Arun <arunsenapati407@gmail.com>
2026-09-09 16:22:15 +0000

    Find Maximum using If-Else

---
+++ C/If_Else/Maximum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int a, b;
+    printf("Enter the Two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    if( a > b){
+        printf("%d is greater than %d\n", a, b);
+    }else{
+        printf("%d is greater than %d\n", b, a);
+    }
+    return 0;
+}

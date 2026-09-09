9104d2f5fcf72262e4cfd7862eac4a5987197371
Arun <arunsenapati407@gmail.com>
2026-09-09 16:32:57 +0000

    Check the number visibility by 5 and 11

---
+++ C/If_Else/check_Divisibility.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    if(num % 5 == 0 && num % 11 == 0){
+        printf("This Number is divisible by 5 and 11.\n");
+    }else{
+        printf("This Number is not divisible by 5 and 11. \n");
+    }
+    return 0;
+}
